4647717f0e3487a78225dbcb5d06d64b4bfb9b50
MANUEL SANTIAGO MENDOZA <42306132@fi.unju.edu.ar>
2024-11-25 14:16:54 -0300

    integracion de backups y avance en la creacion de La Replidcacion en Mongo

---
+++ docker_compose/scripts/mongo_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Espera para garantizar que los contenedores de MongoDB estén listos
+sleep 10
+
+# Ejecuta el comando para iniciar el conjunto de réplicas
+mongosh --host mongo1:27017 <<EOF
+  var cfg = {
+    "_id": "myReplicaSet",
+    "version": 1,
+    "members": [
+      {
+        "_id": 0,
+        "host": "mongo1:27017",
+        "priority": 2
+      },
+      {
+        "_id": 1,
+        "host": "mongo2:27017",
+        "priority": 0
+      },
+      {
+        "_id": 2,
+        "host": "mongo3:27017",
+        "priority": 0
+      }
+    ]
+  };
+  rs.initiate(cfg);
+EOF
